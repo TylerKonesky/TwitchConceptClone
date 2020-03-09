ccf8a47e495a3c0b31d9aa2062820cd42941ab33
TylerKonesky <tylerkonesky@hotmail.com>
2020-03-09 13:29:34 -0600

    Added redux forms and form validation for createStreatm

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers/index'
 
-const store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+    )
 
 ReactDOM.render(
 
